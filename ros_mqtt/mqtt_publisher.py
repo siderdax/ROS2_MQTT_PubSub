541b00e8114d22e5e2061e5648a2ed3a0d9c7985
withcontrolL1 <headwaving@gmail.com>
2024-07-03 11:19:39 +0900

    Add mqtt_publisher parameters

--- ros_mqtt/mqtt_publisher.py
+++ ros_mqtt/mqtt_publisher.py
@@ -1,24 +1,59 @@
+from typing import List
 import rclpy
 from rclpy.node import Node
+from rclpy import Parameter
+from rcl_interfaces.msg import SetParametersResult
 
 from std_msgs.msg import String
 
 import paho.mqtt.client as mqtt
 
+
 class MqttPublisher(Node):
-    topic = "topic"
+    mqtt_config = {"host": "localhost", "port": 1883}
+    topic = "ros_mqtt"
 
     def __init__(self):
         super(MqttPublisher, self).__init__("mqtt_pub")
-        self.get_logger().debug(f"topic name: {self.topic}")
+        self.declare_parameters(
+            "mqtt_config",
+            [
+                ("host", "localhost"),
+                ("port", 1883),
+                ("topic", "ros_mqtt"),
+            ],
+        )
+        self.add_on_set_parameters_callback(self.on_set_parameters)
         self.mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
         self.mqttc.on_connect = self.on_connect
         self.mqttc.on_disconnect = self.on_disconnect
-        self.mqttc.connect("localhost")
+        self.mqttc.connect(self.mqtt_config["host"], self.mqtt_config["port"])
         self.subscribtion = self.create_subscription(
             String, "mqtt_pub_message", self.listener_callback, 10
         )
         self.subscribtion
+
+    def on_set_parameters(self, params: List[Parameter]):
+        for param in params:
+            config_name = param.name.replace("mqtt_config.", "")
+            if (
+                config_name in self.mqtt_config
+                and self.mqtt_config[config_name] != param.value
+            ):
+                self.get_logger().info(
+                    f"config {config_name} is changed from {self.mqtt_config[config_name]} to {param.value}"
+                )
+                self.mqtt_config[config_name] = param.value
+                self.mqttc.disconnect()
+                self.mqttc.connect(self.mqtt_config["host"], self.mqtt_config["port"])
+
+            if config_name == "topic" and self.topic != param.value:
+                self.get_logger().info(
+                    f"topic is changed from {self.topic} to {param.value}"
+                )
+                self.topic = param.value
+
+        return SetParametersResult(successful=True)
 
     def listener_callback(self, msg: String):
         self.mqttc.loop_start()
@@ -32,8 +67,7 @@ class MqttPublisher(Node):
                 f"Failed to connect: {reason_code}. loop_forever() will retry connection"
             )
         else:
-            self.get_logger().debug(f"mqttc connected, subscribe {self.topic}")
-            client.subscribe(self.topic)
+            self.get_logger().debug(f"mqttc connected")
 
     def on_disconnect(self, client, userdata, flags, reason_code, properties):
         self.get_logger().debug("disconnect mqttc")

721abeea8f1a68e841f96630949d0bb61946796a
withcontrolL1 <headwaving@gmail.com>
2024-07-10 17:16:31 +0900

    add node name arg

--- launch/mqtt.launch.py
+++ launch/mqtt.launch.py
@@ -6,18 +6,21 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    node_name = LaunchConfiguration("node_name")
     role = LaunchConfiguration("role")
     host = LaunchConfiguration("host")
     port = LaunchConfiguration("port")
     topic = LaunchConfiguration("topic")
 
+    node_name_larg = DeclareLaunchArgument("node_name", default_value="ros_mqtt_sub")
     role_larg = DeclareLaunchArgument("role", default_value="subscriber")
     host_larg = DeclareLaunchArgument("host", default_value="localhost")
-    port_larg = DeclareLaunchArgument("port", default_value='1883')
+    port_larg = DeclareLaunchArgument("port", default_value="1883")
     topic_larg = DeclareLaunchArgument("topic", default_value="ros_mqtt_topic")
 
     mqtt_node = Node(
         package="ros_mqtt",
+        name=node_name,
         executable=role,
         parameters=[
             {
@@ -30,4 +33,6 @@ def generate_launch_description():
         ],
     )
 
-    return LaunchDescription([role_larg, host_larg, port_larg, topic_larg, mqtt_node])
+    return LaunchDescription(
+        [node_name_larg, role_larg, host_larg, port_larg, topic_larg, mqtt_node]
+    )
